a7af09757191adfebfa0a72935303ddc8f7005e7
kanavhetiray <kanavhetiray@gmail.com>
2018-02-21 07:26:53 +0200

    added a course model and courses controller api

--- Abeced.UI.Web/Controllers/AppController.cs
+++ Abeced.UI.Web/Controllers/AppController.cs
@@ -56,25 +56,13 @@ namespace Abeced.UI.Web.Controllers
             }
             return View();
 
-            //MultipartFormDataContent content = new MultipartFormDataContent();
-            //byte[] Bytes = new byte[qImageUpload.InputStream.Length + 1];
-            //qImageUpload.InputStream.Read(Bytes, 0 , Bytes.Length);
-            //var fileContent = new ByteArrayContent(Bytes);
-            //fileContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment") { FileName =qImageUpload.FileName};
+        }
 
-            //byte[] Bytes2 = new byte[aImageUpload.InputStream.Length + 1];
-            //aImageUpload.InputStream.Read(Bytes2, 0, Bytes2.Length);
-            //var fileContent2 = new ByteArrayContent(Bytes2);
-            //fileContent2.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment") { FileName = aImageUpload.FileName };
-            //content.Add(fileContent);
-            //content.Add(fileContent2);
-            //fact.aImage = content;
-            //fact.aImage = content2;
+        public ActionResult AddorEditCourse()
+        {
 
-            // HttpResponseMessage response = DataAccess.WebClient.PostAsJsonAsync("facts", fact).Result;
 
-            // return RedirectToAction("Login", "account");
-            //return Json(new { success = true, html = "", message = "success" }, JsonRequestBehavior.AllowGet);
+            return View();
         }
 
     }
